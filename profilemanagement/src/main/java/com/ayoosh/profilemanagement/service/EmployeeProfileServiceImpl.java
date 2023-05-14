dd4f2adcf12911092f59b781c2272400d14c5de8
Asus <phucanhprono1@gmail.com>
2023-05-14 21:40:17 +0700

    Initial commit

---
+++ profilemanagement/src/main/java/com/ayoosh/profilemanagement/service/EmployeeProfileServiceImpl.java
@@ -0,0 +1,26 @@
+package com.ayoosh.profilemanagement.service;
+
+import com.ayoosh.profilemanagement.domain.EmployeeProfile;
+import com.ayoosh.profilemanagement.repository.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeProfileServiceImpl implements EmployeeProfileService {
+
+    @Autowired
+    ProfileRepository repository;
+    List <EmployeeProfile> employeeProfileList = new ArrayList < > ();
+
+    @Override
+    public void addEmployeeProfile(EmployeeProfile profile) {
+        repository.save(profile);
+    }
+
+    @Override
+    public List < EmployeeProfile > getEmployeeProfiles() {
+        return repository.findAll();
+    }
+}
